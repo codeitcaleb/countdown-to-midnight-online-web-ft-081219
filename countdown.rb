ee0267e5f4ef175cd03eed7e7df1be937bcf1500
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-14 19:42:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+  counter = 10 
+  
+  while counter <= integer do
+    puts "#{integer}SECOND(S)!"
+    counter -= 1
+  end  
+  
+  puts "HAPPY NEW YEAR!"
 end
+  
+
+def countdown_with_sleep(integer)
+  countdown sleep(1)
+end  
